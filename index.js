d71c34da900a16a6c3dba45145ca1ac884f0a944
tamrat teare <tamrat33@gmail.com>
2017-07-26 19:03:31 -0400

    intial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require ("express");
+const express = ();
+// const bodyParser = require ( "body-parser");
+// const expressValidator = require (express-validator);
+// const mustacheExpress = require('mustache-express');
